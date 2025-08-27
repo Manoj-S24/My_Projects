eeb1fa94efaa134c801882e6804baeef7ab8f0a9
Manoj S <manojs24narasimha@gmail.com>
2025-08-27 10:31:38 +0530

    Made calculator function using arrow function and etc

---
+++ Calculator/script.js
@@ -0,0 +1,27 @@
+let val="";
+let buttons=document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML==" = "){
+            val=eval(val);
+          document.querySelector('input').value= val ;
+        }
+        else if(e.target.innerHTML=="AC"){
+            val="";
+            document.querySelector('input').value=val;
+        }
+        else if(e.target.innerHTML=="C"){
+            val=val.substring(0 ,val.length-1);
+            document.querySelector('input').value= val ;
+        }
+
+        
+        else{console.log(e.target.innerHTML);
+        val= val + e.target.innerHTML;
+        document.querySelector('input').value= val ;
+        }
+
+        
+    })
+    
+})
